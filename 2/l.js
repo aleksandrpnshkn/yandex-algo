7d6a0354fb96db25afec38134da99e5179a7c1a0
aleksandr <aleksandrkrzhn@yandex.ru>
2021-12-09 20:47:55 +0400

    кэшировать

--- 2/l.js
+++ 2/l.js
@@ -15,18 +15,24 @@ process.stdin.on('end', solve);
 function solve() {
     const [traineeIndex, numberOfDigits] = readArray();
 
-    const numberOfCommits = fib(traineeIndex);
+    const numberOfCommits = fib(Number(traineeIndex));
     const rem = numberOfCommits % (10 ** numberOfDigits);
 
     process.stdout.write(String(rem));
 }
+
+const cache = {};
 
 function fib(n) {
     if (n === 0 || n === 1) {
         return 1;
     }
 
-    return fib(n - 1) + fib(n - 2);
+    if (cache[n]) {
+        return cache[n];
+    }
+
+    return cache[n] = fib(n - 1) + fib(n - 2);
 }
 
 function readArray() {
