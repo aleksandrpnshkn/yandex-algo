7530cbc174d3d4df1d573676c0260411c4ebce28
aleksandr <aleksandrkrzhn@yandex.ru>
2022-01-07 01:54:26 +0400

    use bubble in class

--- 3/o.js
+++ 3/o.js
@@ -12,77 +12,39 @@ reader.on('line', (line) => {
 
 process.stdin.on('end', solve);
 
-class Diffs {
-    constructor(maxSize) {
-        this._items = [];
-        this._maxSize = maxSize;
-    }
-
-    push(diff) {
-        if (this.isFull()) {
-            if (diff >= this.getMax()) {
-                return;
-            }
-
-            this._items[0] = diff;
-
-            for (let i = 1; i < this.size(); i++) {
-                if (this._items[i] <= this._items[i-1]) {
-                    break;
-                }
-
-                [this._items[i-1], this._items[i]] = [this._items[i], this._items[i-1]];
-            }
-        } else {
-            this._items.push(diff);
-
-            for (let i = this.size() - 1; i > 0; i--) {
-                if (this._items[i] <= this._items[i-1]) {
-                    break;
-                }
-
-                [this._items[i-1], this._items[i]] = [this._items[i], this._items[i-1]];
-            }
-        }
-    }
-
-    getMax() {
-        if (this.isEmpty()) {
-            return -Infinity;
-        }
-
-        return this._items[0];
-    }
-
-    size() {
-        return this._items.length;
-    }
-
-    isEmpty() {
-        return this._items.length === 0;
-    }
-
-    isFull() {
-        return this.size() >= this._maxSize;
-    }
-}
-
 function solve() {
     readNumber();
     const areas = readNumericArray();
     const k = readNumber();
 
-    const diffs = new Diffs(k);
+    const diffs = [];
 
     for (let i = 0; i < areas.length; i++) {
-        for (let j = areas.length - 1; j >= i + 1; j--) {
-            const diff = Math.abs(areas[i] - areas[j]);
-
-            diffs.push(diff);
+        for (let j = i + 1; j < areas.length; j++) {
+            diffs.push(Math.abs(areas[i] - areas[j]));
         }
     }
 
-    process.stdout.write(String(diffs.getMax()) + '\n');
+    bubbleSort(diffs);
+
+    process.stdout.write(String(diffs[k - 1]) + '\n');
+}
+
+function bubbleSort(arr) {
+    let hasChanges = true;
+
+    while (hasChanges) {
+        hasChanges = false;
+
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i-1] > arr[i]) {
+                [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
+                hasChanges = true;
+            }
+        }
+    }
+
+    return arr;
 }
 
 function readNumber() {
