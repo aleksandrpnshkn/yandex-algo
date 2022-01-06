0b3b4b9faa9a6d10bbd01c7cba2c403f210cdce4
aleksandr <aleksandrkrzhn@yandex.ru>
2022-01-06 21:01:40 +0400

    ML

--- 3/o.js
+++ 3/o.js
@@ -12,75 +12,59 @@ reader.on('line', (line) => {
 
 process.stdin.on('end', solve);
 
-class Diffs {
-    constructor(maxSize) {
-        this._sortedDiffs = [];
-        this._maxSize = maxSize;
-    }
-
-    push(diff) {
-        if (this.isFull()) {
-            if (diff > this.getMax()) {
-                return;
-            }
-
-            this._sortedDiffs.shift();
-        }
-
-        if (diff < this.getMin()) {
-            this._sortedDiffs.push(diff);
-            return;
-        }
-
-        this._sortedDiffs.push(diff);
-        this._sortedDiffs.sort((a, b) => b - a);
-    }
-
-    getMax() {
-        if (this.isEmpty()) {
-            return -Infinity;
-        }
-
-        return this._sortedDiffs[0];
-    }
-
-    getMin() {
-        if (this.isEmpty()) {
-            return +Infinity;
-        }
-
-        return this._sortedDiffs[this.size() - 1];
-    }
-
-    size() {
-        return this._sortedDiffs.length;
-    }
-
-    isEmpty() {
-        return this._sortedDiffs.length === 0;
-    }
-
-    isFull() {
-        return this.size() >= this._maxSize;
-    }
-}
-
 function solve() {
     readNumber();
     const areas = readNumericArray();
     const k = readNumber();
 
-    const diffs = new Diffs(k);
+    let diffs = [];
+
+    areas.sort((a, b) => a - b);
 
     for (let i = 0; i < areas.length; i++) {
-        for (let j = areas.length - 1; j >= i + 1; j--) {
+        const currDiffs = [];
+
+        for (let j = i + 1; j < areas.length; j++) {
             const diff = Math.abs(areas[i] - areas[j]);
 
-            diffs.push(diff);
+            if (currDiffs.length < k) {
+                currDiffs.push(diff);
+            }
+        }
+
+        diffs = mergeSorted(diffs, currDiffs, k);
+    }
+
+    process.stdout.write(String(diffs[k - 1]) + '\n');
+}
+
+function mergeSorted(arr1, arr2, maxSize) {
+    const merged = [];
+
+    let i = 0;
+    let k = 0;
+
+    while (i < arr1.length && k < arr2.length && merged.length < maxSize) {
+        if (arr1[i] <= arr2[k]) {
+            merged.push(arr1[i]);
+            i++;
+        } else {
+            merged.push(arr2[k]);
+            k++;
         }
     }
 
-    process.stdout.write(String(diffs.getMax()) + '\n');
+    while (i < arr1.length && merged.length < maxSize) {
+        merged.push(arr1[i]);
+        i++;
+    }
+
+    while (k < arr2.length && merged.length < maxSize) {
+        merged.push(arr2[k]);
+        k++;
+    }
+
+    return merged;
 }
 
 function readNumber() {
